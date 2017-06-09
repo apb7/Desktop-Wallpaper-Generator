1bf00b9abbc598483577a3f570996427f0580d42
Apurv Bajaj <apurvabajaj007@gmail.com>
2017-06-09 21:09:35 +0530

    Add files via upload

---
+++ desk_wall.py
@@ -0,0 +1,30 @@
+import requests,random
+import os
+#wp_num=raw_input("Enter the wallpaper's number> ")   #In case of user-input!
+wp_num=str(random.randint(1,999999)) #NSFW
+#if int(wp_num)>999999 and int(wp_num)<1:             #To be used in case of user input! 
+#    print "The URL does not exists!"
+#    exit(0)
+res_jpg=requests.get("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"+wp_num+".jpg")
+#print str(res_jpg.status_code)
+while True:
+    if res_jpg.status_code==requests.codes.ok:
+        fp=open("img_wall_desktop.jpg",'wb')
+        for data in res_jpg.iter_content(100000):
+            fp.write(data)
+        fp.close()
+        wall_path=os.path.abspath("img_wall_desktop.jpg")
+        break
+    else:
+        res_png=requests.get("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"+wp_num+".png")
+        if res_png.status_code==requests.codes.ok:
+            fp=open("img_wall_desktop.png",'wb')
+            for data in res_png.iter_content(100000):
+                fp.write(data)
+            fp.close()
+            wall_path=os.path.abspath("img_wall_desktop.png")
+            break 
+    wp_num=str(random.randint(1,999999)) #NSFW        
+    res_jpg=requests.get("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"+wp_num+".jpg")
+os.system("gsettings set org.gnome.desktop.background picture-uri file://"+wall_path) 
+print "Your wallpaper has been set!"
